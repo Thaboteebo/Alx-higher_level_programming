885c188410d8ee5061ce66930da8a3915e14fc7e
Thaboteebo <Thaboteebo01@gmail.com>
2024-01-09 05:27:06 -0800

    Build my own calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
+    import sys
     from calculator_1 import add, sub, mul, div
-    from sys import argv
-    num_args = len(argv)
-    if num_args != 4:
+    if len(sys.argv != 4:
         print("Usage: ./100-my_calculator.py <a> operator <b>")
         exit(1)
-    a = int(argv[1])
-    operator = argv[2]
-    b = int(argv[3])
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    operator = sys.argv[2]
     if operator == '+':
         print(f"{a} {operator} {b} = {add(a, b)}")
     elif operator == '-':
