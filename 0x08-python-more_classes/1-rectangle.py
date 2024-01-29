ab8fea6c4318e77debef98fc60688ccd2f25496a
Thaboteebo <Thaboteebo01@gmail.com>
2024-01-29 09:36:51 -0800

    Real definition of a rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -35,6 +35,6 @@ class Rectangle:
         """setter for the private instance attribute height"""
         if type(value) is not int:
             raise TypeError("height must be an integer")
-            if value < 0:
-                raise ValueError("height must be >= 0")
-            self.__height = value
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
