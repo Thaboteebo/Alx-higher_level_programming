945fed69975747f9d0fcd3f5be345ce5df498d9f
Thaboteebo <Thaboteebo01@gmail.com>
2024-01-29 02:08:37 -0800

    Real definition of a rectangle

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Defines a class Rectangle
+Defines a class Rctangle
 """
 
 
@@ -8,8 +8,8 @@ class Rectangle:
     """Representation of a rectangle"""
     def __init__(self, width=0, height=0):
         """Initializes the rectangle"""
-        self.height = height
         self.width = width
+        self.height = height
 
     @property
     def width(self):
@@ -18,7 +18,7 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        """setter for the private instance attribute width"""
+        """setter for the private instance attributes width"""
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
@@ -35,6 +35,16 @@ class Rectangle:
         """setter for the private instance attribute height"""
         if type(value) is not int:
             raise TypeError("height must be an integer")
-            if value < 0:
-                raise ValueError("height must be >= 0")
-            self.__height = value
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """returns the area of the rectangle"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """returns the perimeter of the rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return self.__width * 2) + (self.__height * 2)
