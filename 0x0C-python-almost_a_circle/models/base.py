45540446e89adb8fc625493e8fde9c0c3bf5acae
Thaboteebo <Thaboteebo01@gmail.com>
2024-02-13 01:04:22 -0800

    Base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 '''Module for Base class.'''
-from json import dumps, loads
-import csv
 
 
 class Base:
-    '''A representation of the base of our OPP hierachy.'''
+    '''A representation of the base of our OPP hierarchy.'''
 
     __nb_objects = 0
 
@@ -16,65 +14,3 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-
-    @staticmethod
-    def to_json_string(list_dictionaries):
-        '''Jsonifies a dictionary so it's quite rightly and longer.'''
-        if list_dictionaries is None or not list_dictionaries:
-            return "[]"
-        else:
-            return dumps(list_distionaries)
-
-    @staticmethod
-    def from_json_string(json_string):
-        '''Unjsonifies a dictionary.'''
-        if json_string is None or not json_string:
-            return []
-        return loads(json_string)
-
-    @classmethod
-    def save_to_file(cls, list_objs):
-        '''Saves jsonified object to file.'''
-        if list_obj is not None:
-            list_objs = [o.to_dictionary() for o in list_objs]
-        with open("{}.json".format(cls.__name__), "w", encoding="utf-8") as f:
-            f.write(cls.to_json_string(list_objs))
-
-    @classmethod
-    def create(cls, **dictionary):
-        '''Loaads instance from dictionary.'''
-        from models.rectangle import Rectangle
-        from models.square import Square
-        if cls is Rectangle:
-            new = Rectangle(1, 1)
-        elif cls is Square:
-            new = Square(1)
-        else:
-            new = None
-        new.update(**dictionary)
-        return new
-
-    @classmethod
-    def load_from_file(cls):
-        '''Loads string from file and unjsonified.'''
-        from os import path
-        file = "{}.json".format(cls.__name__)
-        if not path.isfile(file):
-            return []
-        with open(file, "r", encoding="utf-8") as f:
-            return [cls.create(**d) for d in cls.from_json_string(f.read())]
-
-        @classmethod
-        def save_to_file_csv(cls, list_objs):
-            '''Saves object to csv file.'''
-            from models.rectangle import Rectangle
-            from models.square import Square
-            if list_objs is not None:
-                if cls is Rectangle:
-                    list_objs = [[o.id, o.width, o.height, o.x, o.y]
-                            for o in list_objs]
-                else:
-                    list_objs = [[o.id, o.size, o.x, o.y]
-                            for o in list_objs]
-            with open('{}.csv'.format(cls.__name__), 'w', newline='',
-                    encoding='utf-8') as f:
