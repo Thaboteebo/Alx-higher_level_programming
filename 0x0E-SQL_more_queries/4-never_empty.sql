bbf78374766ef8ac060271af251d0ee79d6d5b96
Thaboteebo <Thaboteebo01@gmail.com>
2024-03-13 09:29:37 -0700

    SQL More Queries

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,3 +1,3 @@
 -- creates the table id_not_not on your MySQL server
--- create a table
-CREATE A TABLE IF NOT id_not null (id INT DEFAULT 1, name VARCHAR(256));
+-- creates a table
+CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1, name VARCHAR(256));
